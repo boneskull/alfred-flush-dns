ee18653a48b849167fa2a24cc5b49a43e3960f9b
Christopher Hiller <boneskull@boneskull.com>
2023-01-04 13:23:36 -0800

    feat: notifications

--- flush.sh
+++ flush.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+set -o nounset
+set -e
+_ME="$(basename "${0}")"
+trap 'echo "${_ME} failed on line ${LINENO}. Exit code: ${?}" >&2' ERR
+set -o pipefail
 
 dscacheutil -flushcache
 killall -HUP mDNSResponder
-
+echo 'ok'
